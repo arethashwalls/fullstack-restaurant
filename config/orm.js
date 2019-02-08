f43ad411c4c812c20dd3a11a36226f04365ffe56
ASH Walls <admin@arethawalls.com>
2019-02-08 13:36:25 -0700

    Blocked null values

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,7 @@ module.exports = {
         return new Promise((resolve, reject) => {
             connection.query('SELECT * FROM ??', [table], function (err, data) {
                 console.log(`\nQuerying: ${this.sql}`);
-                if(err) {
+                if (err) {
                     console.log('\norm.selectAll was unsuccessful. See error below.\n');
                     reject(err);
                 }
@@ -18,22 +18,30 @@ module.exports = {
         return new Promise((resolve, reject) => {
             const colNames = Object.keys(valueObj);
             const valNames = Object.values(valueObj);
-            connection.query(`INSERT INTO ?? (??) VALUES (?)`, [table, colNames, valNames], function (err, data) {
-                console.log(`\nQuerying: ${this.sql}`);
-                if(err) {
-                    console.log('\norm.insertOne was unsuccessful. See error below.\n');
-                    reject(err);
+            valNames.forEach(val => {
+                if (!val || val === '') {
+                    console.log('\n\norm.insertOne was unsuccessful.\n\nNull values are forbidden.');
+                    reject('\nNull values are forbidden.');
+                } else {
+                    connection.query(`INSERT INTO ?? (??) VALUES (?)`, [table, colNames, valNames], function (err, data) {
+                        console.log(`\nQuerying: ${this.sql}`);
+                        if (err) {
+                            console.log('\norm.insertOne was unsuccessful. See error below.\n');
+                            reject(err);
+                        }
+                        console.log('\norm.insertOne was successful. See data below.\n')
+                        resolve(data);
+                    });
                 }
-                console.log('\norm.insertOne was successful. See data below.\n')
-                resolve(data);
-            });
+            })
+
         });
     },
     updateOne: (table, valueObj, conditionObj) => {
         return new Promise((resolve, reject) => {
             connection.query('UPDATE ?? SET ? WHERE ?', [table, valueObj, conditionObj], function (err, data) {
                 console.log(`\nQuerying: ${this.sql}`);
-                if(err) {
+                if (err) {
                     console.log('\norm.updateOne was unsuccessful. See error below.\n');
                     reject(err);
                 }
