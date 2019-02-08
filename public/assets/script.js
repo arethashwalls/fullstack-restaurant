9c65c1efec527c6a6080969b768b4f2c22b2c083
ASH Walls <admin@arethawalls.com>
2019-02-08 12:40:41 -0700

    Created PUT request code, functionified ajax calls

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,11 +1,20 @@
+const ajax = (method, url, data) => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => location.reload();
+    request.open(method, url);
+    request.setRequestHeader('Content-Type', 'application/json');
+    console.log(request)
+    request.send(data);
+}
+
 Array.from(document.getElementsByClassName('chow-button'))
 .forEach(button => button.addEventListener('click', function () {
-    console.log(this.dataset.pancakeid);
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = () => {
-        location.reload();
-    };
-    request.open('PUT', `/api/pancakes/${this.dataset.pancakeid}`);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
+    const pancakeId = this.dataset.pancakeid;
+    ajax('PUT', `/api/pancakes/${pancakeId}`, '');
 }));
+
+document.querySelector('#submit').addEventListener('click', event => {
+    event.preventDefault();
+    const newName = document.querySelector('input[type="text"]').value.trim();
+    ajax('POST', '/api/pancakes', JSON.stringify({"pancake_name": newName}));
+});
