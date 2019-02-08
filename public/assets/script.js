fde344b2d69d9f47468c86be5441bc62983ab9e7
ASH Walls <admin@arethawalls.com>
2019-02-08 12:21:02 -0700

    Finished front-end of put request

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,3 +1,11 @@
-document.querySelector('.chow-button').addEventListener('click', () => {
-    
-})
+Array.from(document.getElementsByClassName('chow-button'))
+.forEach(button => button.addEventListener('click', function () {
+    console.log(this.dataset.pancakeid);
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+        location.reload();
+    };
+    request.open('PUT', `/api/pancakes/${this.dataset.pancakeid}`);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send();
+}));
