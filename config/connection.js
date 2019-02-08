43337c7894dc355bc6f5d42a0f4a447afcf98d8f
ASH Walls <admin@arethawalls.com>
2019-02-08 15:42:41 -0700

    Set up remote db variables

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'Fallen Falling',
-    database: 'fullstack_db'
-});
+    user: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'Fallen Falling',
+    database: process.env.DB_NAME || 'fullstack_db'
+})
 
 connection.connect((err) => {
-    if(err) return console.log(err.stack);
+    if (err) return console.log(err.stack);
     console.log(`\nConnected as ID ${connection.threadId}.`)
 })
 
